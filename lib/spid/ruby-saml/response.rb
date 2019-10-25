3e6f0cf54b25f081531f870060ccdc1ee5841504
Fabiano Pavan <fabiano.pavan@soluzionipa.it>
2019-10-25 17:51:45 +0200

    Permesso di non avere il format dell'issuer nella response, deve esserci su asserzione

--- lib/spid/ruby-saml/response.rb
+++ lib/spid/ruby-saml/response.rb
@@ -160,19 +160,23 @@ module Spid
               return (soft ? false : validation_error("Issuer of the Assertion not found or multiple."))
             end
 
-            nodes = issuer_response_nodes + issuer_assertion_nodes
+            issuer_response_nodes.each{ |iss|
+              #controllo: L'attributo Format di Issuer deve essere presente con il valore urn:oasis:names:tc:SAML:2.0:nameid-format:entity
+              return (soft ? false : validation_error("Elemento Issuer non ha formato corretto ")) if !iss.attributes['Format'].nil? && iss.attributes['Format'] != 'urn:oasis:names:tc:SAML:2.0:nameid-format:entity'
 
-            nodes.each{ |iss|
+            }
+
+            issuer_assertion_nodes.each{ |iss|
               #controllo: L'attributo Format di Issuer deve essere presente con il valore urn:oasis:names:tc:SAML:2.0:nameid-format:entity
               return (soft ? false : validation_error("Elemento Issuer non ha formato corretto ")) if iss.attributes['Format'] != 'urn:oasis:names:tc:SAML:2.0:nameid-format:entity'
 
             }
 
+            nodes = issuer_response_nodes + issuer_assertion_nodes
 
             nodes.map { |node| Utils.element_text(node) }.compact.uniq
           end
         end
-
 
 
 
